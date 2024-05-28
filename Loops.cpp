4de7388a3a160217a43af89de27bb058205017b5
Shoaib Ahmed <shoaibahmedprogramming@gmail.com>
2024-05-28 12:55:34 +0500

    Add files via upload

---
+++ Loops.cpp
@@ -0,0 +1,194 @@
+#include<iostream>
+
+
+using namespace std;
+int main ()
+{
+	
+/*	long double f;
+	cout<<"Enter number here :";
+	cin>>f;
+	
+	int result =1;
+	while(f>=1){
+		result*=f;
+		f-1;
+	}
+	cout<<result<<endl;
+*/	
+
+
+
+
+
+
+
+
+ /* int base;
+cout<<"Enter the base :";
+cin>>base;
+
+int power;
+cout<<"Enter the power :";
+cin>>power;
+
+int  result=1;
+
+while(power>=1){
+	result*=base;
+	power--;
+}
+
+cout<<result;     
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+  /* int start;
+   cout<<"Enter the No :";
+   cin>>start;
+   int end;
+   cout<<"Enter the last No  :";
+   cin>>end;
+   int sum;
+   
+   do{
+   	   if(start%2==0){
+		  
+   	   sum+=start;}
+   	    start++;
+   }
+      
+      
+while(start<=end);
+
+cout<<"Sum of Even No :"<<sum<<endl;
+
+
+*/
+
+
+
+
+
+
+
+//int  random_num=1+rand()%100;
+//cout<<random_num;
+
+  
+  
+  
+  
+/*  int row=1;
+  int col=1;
+  
+  for(row=1; row<=5; row++){
+  	for(int col=1; col<=5; col++){
+  		if(col==1 || col==5 || row==1 || row==5){
+  			cout<<"*";
+		  }
+		  else{
+		  	cout<<" ";
+		  }
+	  }
+	  cout<<endl;
+  }
+  
+  
+  
+  
+ */ 
+  
+  
+  
+  
+  
+  
+    
+/*int sum=0;
+int num;
+cout<<"Enter the Number :";
+cin>>num;
+ while (num<=100)
+{
+	if(num%5==0){
+	
+	continue;
+	num+=1;
+	}
+	sum+=num;
+	num+=1;
+}
+cout<<"sum is "<<sum;
+
+
+
+*/
+
+
+
+
+/*int num1,num2;
+char oper, i;
+cout<<"Enter num1 here :"<<endl;
+cin>>num1;
+cout<<"Enter the Operator :"<<endl;
+cin>>oper;
+cout<<"Enter the Num2 :"<<endl;
+cin>>num2;
+do{
+	if(oper=='+'){
+		cout<<"Ans "<<num1+num2;
+	}
+	else if(oper=='-'){
+		cout<<"Ans:"<<num1-num2;
+	}
+		else if(oper=='*'){
+		cout<<"Ans:"<<num1*num2;
+	}
+		else if(oper=='/'){
+		cout<<"Ans:"<<num1/num2;
+	}
+}
+while(i=='y' || i=='Y');
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
